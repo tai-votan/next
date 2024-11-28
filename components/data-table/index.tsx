58a767eadf752aae9fd260c736e9a3a56b0aa192
tai-votan <tai.vo@siliconprime.com>
2024-11-28 22:42:50 +0700

    fix

---
+++ components/data-table/index.tsx
@@ -0,0 +1,173 @@
+'use client';
+
+import {
+  ColumnDef,
+  ExpandedState,
+  flexRender,
+  getCoreRowModel,
+  getExpandedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Pagination } from '@/types/common';
+import { DataTablePagination } from '@/components/data-table/pagination';
+import React from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { PAGINATION } from '@/constants/pagination';
+import { cn } from '@/lib/utils';
+import useQueryString from '@/hooks/use-query-string';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  isLoading?: boolean;
+  pagination?: Pagination;
+  pageSizeOptions?: number[];
+  onClickExpendedRow?: boolean;
+  onRowClick?: (row: TData) => void;
+  getSubRows?: (row: TData) => any;
+  expandedRowModel?: (row: TData) => React.ReactNode;
+}
+
+export function DataTable<TData, TValue>({
+  data,
+  columns,
+  isLoading,
+  pagination,
+  onRowClick,
+  getSubRows,
+  onClickExpendedRow,
+  expandedRowModel,
+}: DataTableProps<TData, TValue>) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const createQueryString = useQueryString(searchParams);
+  const [expanded, setExpanded] = React.useState<ExpandedState>({});
+
+  const { limit = PAGINATION.LIMIT, page = 1, total = 0 } = pagination || {};
+
+  const handleChangePage = (page: number) => {
+    router.push(createQueryString({ page, pathname }), {
+      scroll: false,
+    });
+  };
+
+  const table = useReactTable({
+    defaultColumn: {
+      maxSize: 500,
+      minSize: 0,
+    },
+    state: {
+      expanded,
+    },
+    manualExpanding: true,
+    onExpandedChange: setExpanded,
+    getSubRows,
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+  });
+
+  return (
+    <>
+      <div className="rounded-md border mb-4">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  const headerSize = header.getSize();
+                  return (
+                    <TableHead
+                      key={header.id}
+                      style={{
+                        width: headerSize === 150 ? 'auto' : headerSize,
+                      }}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => {
+                return (
+                  <React.Fragment key={row.id}>
+                    <TableRow
+                      data-state={row.getIsSelected() && 'selected'}
+                      className={cn({
+                        'cursor-pointer': onRowClick || onClickExpendedRow,
+                      })}
+                      onClick={() => {
+                        onRowClick?.(row.original);
+                        if (onClickExpendedRow) {
+                          row.toggleExpanded();
+                        }
+                      }}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                    {row.getIsExpanded() && (
+                      <TableRow>
+                        <TableCell colSpan={row.getVisibleCells().length}>
+                          {expandedRowModel?.(row.original)}
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </React.Fragment>
+                );
+              })
+            ) : isLoading ? (
+              Array.from({ length: PAGINATION.LIMIT }, (_, i) => (
+                <TableRow key={`loading-row-${i}`}>
+                  {Array.from({ length: columns.length }, (_, i) => (
+                    <TableCell key={`loading-cell-${i}`}>
+                      <Skeleton className="h-[22px] w-full" />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  Không có kết quả.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      {pagination && (
+        <DataTablePagination
+          pagination={{
+            page,
+            total,
+            limit,
+            totalPage: Math.ceil(total / limit),
+          }}
+          handleChangePage={handleChangePage}
+        />
+      )}
+    </>
+  );
+}
---
+++ components/data-table/pagination.tsx
@@ -0,0 +1,133 @@
+'use client';
+
+import {
+  Pagination as RCPagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPage,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+interface PaginationProps {
+  currentPage?: number;
+  totalPage?: number;
+}
+
+export function Pagination({ currentPage = 1, totalPage = 1 }: PaginationProps) {
+  const pathname = usePathname();
+
+  const pagerList: React.ReactElement[] = [];
+  const pageBufferSize = 1;
+
+  if (totalPage <= 3 + pageBufferSize * 2) {
+    if (!totalPage) {
+      pagerList.push(
+        <PaginationItem>
+          <PaginationPage isActive>1</PaginationPage>
+        </PaginationItem>,
+      );
+    }
+
+    for (let i = 1; i <= totalPage; i += 1) {
+      pagerList.push(
+        <PaginationItem key={i}>
+          {currentPage === i ? (
+            <PaginationPage isActive>{i}</PaginationPage>
+          ) : (
+            <PaginationLink href={pathname + `?page=${i}`}>{i}</PaginationLink>
+          )}
+        </PaginationItem>,
+      );
+    }
+  } else {
+    let left = Math.max(1, currentPage - pageBufferSize);
+    let right = Math.min(currentPage + pageBufferSize, totalPage);
+
+    if (currentPage - 1 <= pageBufferSize) {
+      right = 1 + pageBufferSize * 2;
+    }
+    if (totalPage - currentPage <= pageBufferSize) {
+      left = totalPage - pageBufferSize * 2;
+    }
+
+    for (let i = left; i <= right; i += 1) {
+      pagerList.push(
+        <PaginationItem key={i}>
+          {currentPage === i ? (
+            <PaginationPage isActive>{i}</PaginationPage>
+          ) : (
+            <PaginationLink href={pathname + `?page=${i}`}>{i}</PaginationLink>
+          )}
+        </PaginationItem>,
+      );
+    }
+
+    if (currentPage - 1 >= pageBufferSize * 2 && currentPage !== 1 + 2) {
+      pagerList[0] = React.cloneElement(pagerList[0]);
+
+      pagerList.unshift(
+        <PaginationItem key="ellipsis_prev">
+          <PaginationEllipsis />
+        </PaginationItem>,
+      );
+    }
+
+    if (totalPage - currentPage >= pageBufferSize * 2 && currentPage !== totalPage - 2) {
+      const lastOne = pagerList[pagerList.length - 1];
+      pagerList[pagerList.length - 1] = React.cloneElement(lastOne);
+
+      pagerList.push(
+        <PaginationItem key="ellipsis_next">
+          <PaginationEllipsis />
+        </PaginationItem>,
+      );
+    }
+
+    if (left !== 1) {
+      pagerList.unshift(
+        <PaginationItem key={1}>
+          <PaginationLink href={pathname + `?page=1`}>1</PaginationLink>
+        </PaginationItem>,
+      );
+    }
+    if (right !== totalPage) {
+      pagerList.push(
+        <PaginationItem key={totalPage}>
+          <PaginationLink href={pathname + `?page=${totalPage}`}>{totalPage}</PaginationLink>
+        </PaginationItem>,
+      );
+    }
+  }
+
+  return (
+    <RCPagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href={pathname + `?page=${Math.max(currentPage - 1, 1)}`} />
+        </PaginationItem>
+        {pagerList}
+        {/*{Array.from({ length: totalPage }, (_, i) => (*/}
+        {/*  <PaginationItem key={`PaginationItem-${i + 1}`}>*/}
+        {/*    <PaginationLink*/}
+        {/*      href={pathname + `?page=${i + 1}`}*/}
+        {/*      isActive={page === i + 1}*/}
+        {/*    >*/}
+        {/*      {i + 1}*/}
+        {/*    </PaginationLink>*/}
+        {/*  </PaginationItem>*/}
+        {/*))}*/}
+        {/*<PaginationItem>*/}
+        {/*  <PaginationEllipsis />*/}
+        {/*</PaginationItem>*/}
+        <PaginationItem>
+          <PaginationNext href={pathname + `?page=${Math.min(currentPage + 1, totalPage)}`} />
+        </PaginationItem>
+      </PaginationContent>
+    </RCPagination>
+  );
+}
